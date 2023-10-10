95c3bcbcc32931855873afb5d247ca0487b5eed7
cassmaoo <zm789@nyu.edu>
2023-10-10 00:48:27 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,92 @@
+// Define an array of circles
+let circles = [];
+
+// Canvas setup
+function setup() {
+  createCanvas(800, 600);
+}
+
+// Draw function
+function draw() {
+  // Set background to dark grey
+  background(20);
+
+  // Update and display circles
+  for (let circle of circles) {
+    circle.display();
+    circle.update();
+  }
+}
+
+// Create a new circle on mouse press
+function mousePressed() {
+  // Generate random colors for each circle
+  let circleColor = color(random(255), random(255), random(255));
+
+  // Create a new circle object and add it to the array
+  let newCircle = new Circle(mouseX, mouseY, circleColor);
+  circles.push(newCircle);
+}
+
+// Declare the name
+class Circle {
+
+  // Constructor 
+  constructor(x, y, circleColor) {
+
+    // Constructors of the circles' properties
+    this.pos = createVector(x, y);
+    this.radius = random(10, 60);
+    this.vel = createVector(random(-2, 2), random(-2, 2));
+    this.color = circleColor;
+  }
+
+  // Display the circles
+  display() {
+    fill(this.color);
+    noStroke();
+    ellipse(this.pos.x, this.pos.y, this.radius * 2);
+  }
+
+  // Update the circles
+  update() {
+    this.pos.add(this.vel);
+
+    // Make circles bounce off the edges of the canvas
+    // width
+    if (this.pos.x > width - this.radius || this.pos.x < this.radius) {
+      this.vel.x *= -1;
+    }
+
+    // height
+    if (this.pos.y > height - this.radius || this.pos.y < this.radius) {
+      this.vel.y *= -1;
+    }
+
+    // Collision of other circles
+    for (let other of circles) {
+      if (other !== this) {
+        let distance = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
+        let minDistance = this.radius + other.radius;
+
+        if (distance < minDistance) {
+
+          // Change color on collision
+          this.color = color(random(255), random(255), random(255));
+          other.color = color(random(255), random(255), random(255));
+
+          let angle = atan2(other.pos.y - this.pos.y, other.pos.x - this.pos.x);
+          let targetX = this.pos.x + cos(angle) * minDistance;
+          let targetY = this.pos.y + sin(angle) * minDistance;
+          let ax = (targetX - other.pos.x) * 0.05;
+          let ay = (targetY - other.pos.y) * 0.05;
+          this.vel.x -= ax;
+          this.vel.y -= ay;
+          other.vel.x += ax;
+          other.vel.y += ay;
+        }
+      }
+    }
+  }
+}
+
